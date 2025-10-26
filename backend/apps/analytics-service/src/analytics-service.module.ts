f9cfb6ec039c3ba288b25da0abd2f68c3c78523e
ludwinsubbaiahhh <ludwinsubbaiah745@gmail.com>
2025-10-26 13:29:46 +0530

    feat(analytics-service): setup Prisma module and dependencies

--- backend/apps/analytics-service/src/analytics-service.module.ts
+++ backend/apps/analytics-service/src/analytics-service.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AnalyticsServiceController } from './analytics-service.controller';
 import { AnalyticsServiceService } from './analytics-service.service';
 import { RabbitMQModule } from '../../../../libs/common/src/rabbitmq/rabbitmq.module';
+import { PrismaModule } from './prisma.module';
 
 @Module({
-  imports: [RabbitMQModule],
+  imports: [RabbitMQModule, PrismaModule],
   controllers: [AnalyticsServiceController],
   providers: [AnalyticsServiceService],
 })
