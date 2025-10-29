0dd7bbaebf8b142b211b8c9919649ebf00ea8b2e
ludwinsubbaiahhh <ludwinsubbaiah745@gmail.com>
2025-10-30 02:30:15 +0530

    worker service to connect to database and redis

--- backend/apps/worker-service/src/worker-service.module.ts
+++ backend/apps/worker-service/src/worker-service.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { WorkerServiceController } from './worker-service.controller';
 import { WorkerServiceService } from './worker-service.service';
 import { RabbitMQModule } from '../../../../libs/common/src/rabbitmq/rabbitmq.module';
+import { PrismaModule } from './prisma.module';
+import { RedisModule } from './redis.module';
 
 @Module({
-  imports: [RabbitMQModule],
+  imports: [RabbitMQModule, PrismaModule, RedisModule],
   controllers: [WorkerServiceController],
   providers: [WorkerServiceService],
 })
